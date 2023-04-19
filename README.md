d7f3ac4a88198d9436c70b418d2b073c9574afa4
z <80871202+90th@users.noreply.github.com>
2023-04-18 21:35:07 -0400

    1.0
    published.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SharpLog
  C# Lightweight logger.
+
+
+```
+using SharpLog;
+using System;
+
+namespace SharpLogDemo {
+    class Program {
+        static void Main(string[] args) {
+            SharpLog.SharpLog logger = new SharpLog.SharpLog("log.txt", SharpLog.LogLevel.Trace);
+
+            logger.Trace("This is a trace message6");
+            logger.Debug("This is a debug message5");
+            logger.Info("This is an info message4");
+            logger.Warning("This is a warning message3");
+            logger.Error("This is an error message2");
+            logger.Fatal("This is a fatal message1");
+            Console.WriteLine("Log messages written to log.txt");
+
+        }
+    }
+}
+
+```
---
+++ SharpLog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace SharpLog {
+    public enum LogLevel {
+        Trace,
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Fatal
+    }
+
+    public class SharpLog {
+        private readonly string _logFilePath;
+        private readonly LogLevel _minimumLogLevel;
+
+        public SharpLog(string logFilePath, LogLevel minimumLogLevel) {
+            _logFilePath = logFilePath;
+            _minimumLogLevel = minimumLogLevel;
+        }
+
+        public void Log(LogLevel logLevel, string message) {
+            if (logLevel < _minimumLogLevel) {
+                return;
+            }
+
+            string logMessage = $"{DateTime.UtcNow} - [{logLevel}] > {message}";
+
+            using (StreamWriter writer = File.AppendText(_logFilePath)) {
+                writer.WriteLine(logMessage);
+            }
+        }
+
+        public void Trace(string message) {
+            Log(LogLevel.Trace, message);
+        }
+
+        public void Debug(string message) {
+            Log(LogLevel.Debug, message);
+        }
+
+        public void Info(string message) {
+            Log(LogLevel.Info, message);
+        }
+
+        public void Warning(string message) {
+            Log(LogLevel.Warning, message);
+        }
+
+        public void Error(string message) {
+            Log(LogLevel.Error, message);
+        }
+
+        public void Fatal(string message) {
+            Log(LogLevel.Fatal, message);
+        }
+    }
+}
